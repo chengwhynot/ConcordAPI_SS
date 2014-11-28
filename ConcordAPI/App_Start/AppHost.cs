04f2cea1e983a61758ad99e362e1392c7bfbd010
Cheng Zhang <zhangcheng81@gmail.com>
2014-11-28 13:19:21 +0800

    Init by ServiceStack

---
+++ ConcordAPI/App_Start/AppHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Funq;
+using ServiceStack;
+
+namespace ConcordAPI
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("Concord API by SS", typeof(AppHost).Assembly)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            
+        }
+    }
+}
---
+++ ConcordAPI/Models/AccountModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+
+namespace ConcordAPI.Models
+{
+    [Route("/account","POST")]
+    public class AccountRequest:IReturn<AccountResponse>
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PrimaryPhone{ get; set; }
+        public string PrimaryEmail { get; set; }
+        public string Password { get; set; }
+        public int CompanyId { get; set; }
+        public virtual Company Companys { get; set; }
+    }
+
+    public class AccountResponse
+    {
+        public int Id { get; set; }
+        public ResponseStatus ResponseStatus{get;set;}
+    }
+}
